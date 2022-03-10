38f3561fc5ec523fbafcdb921dde9c144d97140a
IsaiasQB <isaias.qb@hotmail.com>
2022-03-10 09:53:34 -0500

    add users input from the form into the list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,25 @@ var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event){
-
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // create a list item and style it with a class name
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a newtask!";
+
+    //create div to hold task info and add it inside of list item, give it a class
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3>"+
+                            "<span class='task-type'>"+ taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add the entire list item to the ul
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", createTaskHandler);
