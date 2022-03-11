ec58336e752b98f1cf6c1777236d92098d002fc7
IsaiasQB <isaias.qb@hotmail.com>
2022-03-11 00:23:57 -0500

    added custom IDs to each new list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 
 var taskFormHandler = function (event){
     event.preventDefault();
@@ -29,15 +31,25 @@ var createTaskEl = function(taskDataObj) {
     // create a list item and style it with a class name
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
     //create div to hold task info and add it inside of list item, give it a class
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3>" + "<span class='task-type'>"+ taskDataObj.type + "</span>";
-    
     listItemEl.appendChild(taskInfoEl);
 
     // add the entire list item to the ul
     tasksToDoEl.appendChild(listItemEl);
+
+    // increase the task counter for the next uique id
+    taskIdCounter++;
 };
+
+
+// var createTaskActions = fucntion(taskId) {
+
+// }
+
 
 formEl.addEventListener("submit", taskFormHandler);
